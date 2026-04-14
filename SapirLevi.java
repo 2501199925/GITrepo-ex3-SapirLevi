dd4c1a1806fc2dcacb0fc1979a26f4342d65af1f
2501199925 <sapirlevi250@gmail.com>
2026-04-14 11:20:51 +0300

    First commit

--- SapirLevi.java
+++ SapirLevi.java
@@ -1,9 +1,8 @@
-public class Hello {
+public class Sapir {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Hello World ..." + i);
+            System.out.println("SapirLevi");
         }
     }
-
-} 
+}
